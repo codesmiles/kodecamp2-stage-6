b8d315fcc3020f510ee18ca17a456cfa0ad0595f
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-06-07 17:45:42 +0100

    update

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,27 +1,24 @@
-
-// general
+// GENERAL
 module.exports.generalController_get = async(req, res) =>{
   res.send(`<h1>Its suppose to work</h1>`);
 };
 
-// register
+// REGISTER
 module.exports.registerController_get = async (req, res) => {
   res.send(`<h1>new register</h1>`);
-
 };
 module.exports.registerController_post = async (req, res) => {
-  // res.send(`);}
   console.log(`it suppose work`);
 };
 
-// login
+//LOGIN
 module.exports.loginController_get = async (req, res) => {
   res.send(`<h1>Its suppose to work</h1>`);
 };
-
 module.exports.loginController_post = async (req, res) => {
   res.send(`<h1>Its suppose to work</h1>`);
 };
+
 // RESTRICTED
 module.exports.restrictedController_get = async (req, res) => {
   res.send(`<h1>Its suppose to work</h1>`);
